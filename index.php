f1c63b69818320d0f4fce2991a4f4bf0bd9e4e93
alehinoleg1@gmail.com <alehinoleg1@gmail.com>
2019-02-28 23:29:10 +0700

    Задание №6

--- index.php
+++ index.php
@@ -48,6 +48,18 @@ $ad_information = [
         'image_url' => 'img/lot-6.jpg'
     ]
 ];
+
+function formating_number(
+$number)
+    {
+        $int = ceil($number);
+
+        if ($int > 1000){
+            $int = number_format($int, 0, ' ', ' ');
+        }
+        return $int;
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -112,18 +124,18 @@ $ad_information = [
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-        <?php foreach ($ad_information as $key => $value): ?>
+        <?php foreach ($ad_information as $key => $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $value['image_url']; ?>" width="350" height="260" alt="">
+                    <img src="<?= $item['image_url']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $value['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $value['name']; ?></a></h3>
+                    <span class="lot__category"><?= $item['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= $value['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?= formating_number($item['price']); ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -141,9 +153,9 @@ $ad_information = [
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <?php foreach($product_categories as $key => $value): ?>
+            <?php foreach($product_categories as $category): ?>
                 <li class="nav__item">
-                    <a href="pages/all-lots.html"><?= $value; ?></a>
+                    <a href="pages/all-lots.html"><?= $category; ?></a>
                 </li>
             <?php endforeach; ?>
         </ul>
